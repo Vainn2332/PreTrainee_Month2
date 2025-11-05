d61d521178dc43a28fb45e4edb4150b37f5e42d6
Владислав <vladuk2332@gmail.com>
2025-11-05 21:28:12 +0300

    added products list to User model

--- CoreLayer/Entities/User Entities/User.cs
+++ CoreLayer/Entities/User Entities/User.cs
@@ -1,4 +1,6 @@
-﻿namespace PreTrainee_Month2.CoreLayer
+﻿using PreTrainee_Month2.CoreLayer.Product_Entities;
+
+namespace PreTrainee_Month2.CoreLayer
 {
     public class User
     {
@@ -6,5 +8,8 @@
         public string Name { get; set; } = String.Empty;
         public string EmailAddress { get; set; } = String.Empty;
         public string Role { get; set; } = string.Empty;
+
+        //для связи
+        public List<Product> Products { get; set; } = [];
     }
 }
