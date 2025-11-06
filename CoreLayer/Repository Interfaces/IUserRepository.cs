cd4ddf6bbec0b440f9e6f30561b8a3fbef92b6c9
Владислав <vladuk2332@gmail.com>
2025-11-06 17:20:42 +0300

    added user repository

---
+++ CoreLayer/Repository Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PreTrainee_Month2.CoreLayer.Repository_Interfaces
+{
+    public interface IUserRepository:IRepository<User>
+    {
+        public Task GetAllWithProducts();
+        public Task GetWithProducts(int id);
+    }
+}
