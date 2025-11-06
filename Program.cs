1c65d2e541cfd5f167ad10d6642eab656dd8fd05
Владислав <vladuk2332@gmail.com>
2025-11-06 18:23:03 +0300

    added into some DI-s

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using PreTrainee_Month2.ApplicationLayer.Services;
+using PreTrainee_Month2.CoreLayer.Product_Entities;
+using PreTrainee_Month2.CoreLayer.Repository_Interfaces;
+using PreTrainee_Month2.InfrastructureLayer.DataBaseContext;
+using PreTrainee_Month2.InfrastructureLayer.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +14,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<IRepository<Product>, ProductRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+builder.Services.AddTransient<ProductService>();
+builder.Services.AddTransient<UserService>();
+
+builder.Services.AddDbContext<DBContext>(
+    options =>
+    {
+        options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=PreTraineeMonth2_DB;Trusted_Connection=true;"
+        , b => b.MigrationsAssembly("PreTrainee_Month2"));
+    });
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
