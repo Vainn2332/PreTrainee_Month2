ef6ebce0a2190af06b5fb0c1fb3eda4b3a67c13b
Владислав <vladuk2332@gmail.com>
2025-11-11 16:51:34 +0300

    it works with postgre

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddTransient<IUserService,UserService>();
 builder.Services.AddDbContext<DBContext>(
     options =>
     {
-        options.UseSqlServer(@"Server= mssqlserver_container;Database=PreTraineeMonth2_DB;Trusted_Connection=true;"
+        options.UseSqlServer(@"Server=mssqlserver_container;Database=PreTraineeMonth2_DB;UserId=sa;Password=SukaBlyad123;TrustServerCertificate=true;"
         , b => b.MigrationsAssembly("PreTrainee_Month2"));
     });
 var app = builder.Build();
