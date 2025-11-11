a903bf765d250ac841e62b8a74711c3a1a934d8d
Владислав <vladuk2332@gmail.com>
2025-11-11 20:18:44 +0300

    idk why i isnt working

--- Program.cs
+++ Program.cs
@@ -23,8 +23,8 @@ builder.Services.AddTransient<IUserService,UserService>();
 builder.Services.AddDbContext<DBContext>(
     options =>
     {
-        options.UseNpgsql("Host=localhost;UserId=postgres;Password=1234;Database=mydata;"
-        , b => b.MigrationsAssembly("PreTrainee_Month2"));
+        options.UseNpgsql("Host=postgres;Port=5432;Username=postgres;Password=1234;Database=mydata;");
+       // , b => b.MigrationsAssembly("PreTrainee_Month2"));
     });
 var app = builder.Build();
 // Configure the HTTP request pipeline.
