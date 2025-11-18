c6fff1280095541a2d93a302b3642644fd75fc4f
Владислав <vladuk2332@gmail.com>
2025-11-18 09:04:34 +0300

    added SearchService

---
+++ ApplicationLayer/ServiceInterfaces/ISearchService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using PreTrainee_Month2.CoreLayer.Product_Entities;
+
+namespace PreTrainee_Month2.ApplicationLayer.ServiceInterfaces
+{
+    public interface ISearchService
+    {
+        public Task<Product> SearchByNameAsync(string name);
+        public Task<Product> SearchByPriceAsync(decimal price);
+        public Task<Product> SearchByDateOfCreationAsync(DateTime date);
+        public Task<IEnumerable<Product>> FilterByDateOfCreationAsync();
+        public Task<IEnumerable<Product>> FilterByDateOfCreationPriceAsync();
+        public Task<IEnumerable<Product>> FilterByNameAsync();
+
+    }
+}
---
+++ ApplicationLayer/Services/SearchService.cs
@@ -0,0 +1,52 @@
+﻿using PreTrainee_Month2.ApplicationLayer.ServiceInterfaces;
+using PreTrainee_Month2.CoreLayer.Product_Entities;
+
+namespace PreTrainee_Month2.ApplicationLayer.Services
+{
+    public class SearchService : ISearchService
+    {
+        private readonly IProductService _productService;
+        private readonly IUserService _userService;
+
+        public SearchService(IProductService productService, IUserService userService)
+        {
+            _productService = productService;
+            _userService = userService;
+        }
+
+        public async Task<IEnumerable<Product>> FilterByDateOfCreationAsync()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            var outputProducts = products.OrderBy(p => p.DateOfCreation);
+            return outputProducts;
+        }
+
+        public async Task<IEnumerable<Product>> FilterByDateOfCreationPriceAsync()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            var outputProducts = products.OrderBy(p => p.DateOfCreation);
+            return outputProducts;
+        }
+
+        public Task<IEnumerable<Product>> FilterByNameAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+         public async Task<Product> SearchByDateOfCreationAsync(DateTime date)
+         {
+            throw new NotImplementedException();
+         }
+         
+
+        public async Task<Product> SearchByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> SearchByPriceAsync(decimal price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
